9d7fca30f75b40fde2e8f7e4eb24751c90ad0901
Marc España Quinquilla <44228378+Marc1515@users.noreply.github.com>
2023-04-10 13:22:07 +0200

    datos de la apiKey con la ruta guardados

---
+++ src/app/utils/constantUri.ts
@@ -0,0 +1,7 @@
+
+const baseUrl = "https://api.themoviedb.org/3"
+
+export class ConstantUri {
+    public static readonly apiKey="a3cd814f876e29b589ce39fe46a0c305"
+    public static readonly validateWithLogin = "/authentication/token/validate_with_login"
+}
