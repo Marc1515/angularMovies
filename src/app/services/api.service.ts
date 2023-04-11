01c16ba74f21f3df3bdf12da801687ae108837ea
Marc España Quinquilla <44228378+Marc1515@users.noreply.github.com>
2023-04-11 11:03:05 +0200

    Creamos el servicio que hará las peticiones desde una nueva carpeta services

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http"
+import { Observable, map, catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService<T> {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+
+  postService(paramsReq: any): Observable<T> {
+    const headers = new HttpHeaders({
+      accept: "application/json"
+    })
+
+
+    const { url, params, body } = paramsReq
+    const options = {
+      headers,
+      params
+    }
+
+    return this.http.post<T>(url, body, options)
+    .pipe(map(res => res),
+    catchError(this.handlerError)
+    )
+
+  }
+
+  getService(paramsReq: any): Observable<T> {
+    const headers = new HttpHeaders({
+      accept: "application/json"
+    })
+
+
+    const { url, params } = paramsReq
+    const options = {
+      headers,
+      params
+    }  
+
+    return this.http.get<T>(url, options)
+    .pipe(map(res => res),
+    catchError(this.handlerError)
+    )
+
+  }
+
+
+  private handlerError(err: HttpErrorResponse) {
+    console.log(err.error)
+    return throwError(() => err)
+  }
+}
